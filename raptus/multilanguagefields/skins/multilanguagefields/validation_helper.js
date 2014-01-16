839ce5ad0df0dc9dd27d51b7f83a9d511155f32f
Simone Orsi <simahawk@gmail.com>
2014-01-16 14:49:27 +0100

    use "$" instead of "jq"

--- raptus/multilanguagefields/skins/multilanguagefields/validation_helper.js
+++ raptus/multilanguagefields/skins/multilanguagefields/validation_helper.js
@@ -1,29 +1,29 @@
 /* 2.2011
  * raptus.multilanguagefields
- * 
+ *
  * this is a helper script for kss validation. This insert a hidden
  * input field to detect which field was changed as last.
  */
 
 var validation_helper = {
-  
+
   init:function(elm){
-    jq(elm).find('input, textarea').focus(validation_helper.event);
+    $(elm).find('input, textarea').focus(validation_helper.event);
   },
-  
+
   event:function(obj){
-    var form = jq('.kssActive form[name="edit_form"]');
+    var form = $('.kssActive form[name="edit_form"]');
     var input = form.find('input[name="multilanguagefield_validation"]');
     if (!input.length)
       form.append('<input type="hidden" name="multilanguagefield_validation" />');
     input = form.find('input[name="multilanguagefield_validation"]');
-    input.attr('value', jq(obj.target).attr('name'));
+    input.attr('value', $(obj.target).attr('name'));
   }
 }
 
 
-jq('document').ready(function() {
-  jq('.languageMultiplier').each(function() {
+$('document').ready(function() {
+  $('.languageMultiplier').each(function() {
     validation_helper.init(this);
   });
 });
